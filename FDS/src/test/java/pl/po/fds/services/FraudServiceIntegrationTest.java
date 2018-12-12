60e5c36d02a5e1d51a1042c6b233c7f38f6aabad
Snakeyes <snakeyes@interia.pl>
2018-12-12 23:25:23 +0100

    Moved test to new FraudServiceIntegrationTest class.

--- FDS/src/test/java/pl/po/fds/services/FraudServiceIntegrationTest.java
+++ FDS/src/test/java/pl/po/fds/services/FraudServiceIntegrationTest.java
@@ -17,34 +17,25 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class FraudServiceUnitTest {
+public class FraudServiceIntegrationTest {
 
     @Autowired
     private FraudService fraudService;
 
-    private AccountDTO source;
-    private AccountDTO destination;
     private TransferDTO transfer;
 
     @Before
     public void setUp() throws Exception {
         final ClientDTO clientSource = new ClientDTO(1L, "John", "Doe", null);
         final ClientDTO clientDestination = new ClientDTO(2L, "Jane", "Doe", null);
-        source = new AccountDTO(1L, clientSource, 1000.00, Currency.getInstance("PLN"));
-        destination = new AccountDTO(2L, clientDestination, 2000.00, Currency.getInstance("PLN"));
+        final AccountDTO source = new AccountDTO(1L, clientSource, 1000.00, Currency.getInstance("PLN"));
+        final AccountDTO destination = new AccountDTO(2L, clientDestination, 2000.00, Currency.getInstance("PLN"));
         transfer = new TransferDTO(1L, 190.56, source, destination, new Date());
     }
 
-    @Test
-    public void isTransferSuspiciousWithVeryBigAmount_shouldReturnTrue() {
-        transfer.setAmount(15000.00);
-        assertThat(fraudService.isSuspicious(transfer)).isTrue();
+    @Test // Requires Core service up and running
+    public void isTransferSuspiciousWithAllNormalAndValidData_shoutReturnFalse() {
+        assertThat(fraudService.isSuspicious(transfer)).isFalse();
     }
 
-    @Test
-    public void isTransferSuspiciousWithExoticCurrency_shouldReturnTrue() {
-        source.setCurrency(Currency.getInstance("NGN"));
-        destination.setCurrency(Currency.getInstance("NGN"));
-        assertThat(fraudService.isSuspicious(transfer)).isTrue();
-    }
 }
