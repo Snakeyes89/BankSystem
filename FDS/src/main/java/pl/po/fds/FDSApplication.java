192b38614f89179b340cdda47a3507676cc50975
Snakeyes <snakeyes@interia.pl>
2018-12-12 18:33:23 +0100

    Created new module FDS. Set FDS port to 9010.

---
+++ FDS/src/main/java/pl/po/fds/FDSApplication.java
@@ -0,0 +1,12 @@
+package pl.po.fds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FDSApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FDSApplication.class, args);
+    }
+}
