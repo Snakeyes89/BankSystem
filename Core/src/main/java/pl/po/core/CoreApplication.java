fefa55611f8297f2326e8edeba758782ad0ef9d4
Snakeyes <snakeyes@interia.pl>
2018-12-12 20:46:52 +0100

    Added bean modelMapperFactory to CoreApplication. Added two values to properties file: specified this service port and FDS service address.

--- Core/src/main/java/pl/po/core/CoreApplication.java
+++ Core/src/main/java/pl/po/core/CoreApplication.java
@@ -1,12 +1,19 @@
 package pl.po.core;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CoreApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CoreApplication.class, args);
+    }
+
+    @Bean
+    public ModelMapper modelMapperFactory() {
+        return new ModelMapper();
     }
 }
